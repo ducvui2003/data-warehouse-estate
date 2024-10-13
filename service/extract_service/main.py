448b092cc59102777fee38f0e82e80b19c7857a0
Le Anh Duc <ducvui2003@gmail.com>
2024-10-13 20:28:12 +0700

    fix: use pandas to flat data

--- service/extract_service/main.py
+++ service/extract_service/main.py
@@ -2,25 +2,31 @@ from datetime import datetime
 
 from service.extract_service.src.config.setting import LIMIT_PAGE
 from service.extract_service.src.crawler.source_A_1_crawler import SourceA1Crawler
-from service.extract_service.src.util.file_util import write_json_to_csv
+from service.extract_service.src.crawler.source_A_2_crawler import SourceA2Crawler
+from service.extract_service.src.util.file_util import write_json_to_csv, read_file_to_json
 
 
 def run_crawlers():
     run_crawler_source_A_1()
-    # Initialize Source1Crawler
-    # source1_crawler = Source1Crawler()
-    # source1_crawler.setup_driver(headless=True)  # Headless browser option
-    # # source1_crawler.setJwt()
-    # data = source1_crawler.crawl()
-    # # current_date = datetime.now().strftime("%H_%M__%d_%m_%Y")
-    # # filename = f"source_1_{current_date}.csv"
-    # # write_json_to_csv(filename, data)
+    run_crawler_source_A_2()
 
 
 def run_crawler_source_A_1():
-    source1_crawler = SourceA1Crawler(LIMIT_PAGE)
-    print(f"Started crawl at: {source1_crawler.base_url}")
-    source1_crawler.handle()
+    source_1_crawler = SourceA1Crawler(LIMIT_PAGE)
+    print(f"Started crawl at: {source_1_crawler.base_url}")
+    data = source_1_crawler.handle()
+    current_date = datetime.now().strftime("%H_%M__%d_%m_%Y")
+    filename = f"source_a_2_{current_date}.csv"
+    write_json_to_csv(filename, data)
+
+
+def run_crawler_source_A_2():
+    source_a_2_crawler = SourceA2Crawler(LIMIT_PAGE)
+    print(f"Started crawl at: {source_a_2_crawler.base_url}")
+    data = source_a_2_crawler.handle()
+    current_date = datetime.now().strftime("%H_%M__%d_%m_%Y")
+    filename = f"source_a_2_{current_date}.csv"
+    write_json_to_csv(filename, data)
 
 
 if __name__ == "__main__":

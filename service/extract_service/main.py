4bbcbdb17b9bc51520c09b74537c4b75ea810ac5
Lê Anh Đức <ducvui2003@gmail.com>
2024-10-14 18:39:54 +0700

    Feature/extract service ref (#3)
    * ref: base paging in crawler data
    
    * fix: use pandas to flat data
    
    * fix: use pandas to flat data
    
    * remove duplicate code

--- service/extract_service/main.py
+++ service/extract_service/main.py
@@ -1,17 +1,30 @@
 from datetime import datetime
 
-from service.extract_service.src.crawler.source_1_crawler import Source1Crawler
-from service.extract_service.src.util.file_handler import write_json_to_csv
+from service.extract_service.src.config.setting import LIMIT_PAGE, SOURCE_A_BASE, SOURCE_A_1, SOURCE_A_2
+from service.extract_service.src.crawler.source_A_crawler import SourceACrawler
+from service.extract_service.src.util.file_util import write_json_to_csv
 
 
 def run_crawlers():
-    # Initialize Source1Crawler
-    source1_crawler = Source1Crawler()
-    source1_crawler.setup_driver(headless=True)  # Headless browser option
-    # source1_crawler.setJwt()
-    data = source1_crawler.crawl()
+    run_crawler_source_A_1()
+    run_crawler_source_A_2()
+
+
+def run_crawler_source_A_1():
+    source_1_crawler = SourceACrawler(LIMIT_PAGE, SOURCE_A_BASE, SOURCE_A_1)
+    print(f"Started crawl at: {source_1_crawler.base_url}")
+    data = source_1_crawler.handle()
     current_date = datetime.now().strftime("%H_%M__%d_%m_%Y")
-    filename = f"source_1_{current_date}.csv"
+    filename = f"source_a_2_{current_date}.csv"
+    write_json_to_csv(filename, data)
+
+
+def run_crawler_source_A_2():
+    source_a_2_crawler = SourceACrawler(LIMIT_PAGE, SOURCE_A_BASE, SOURCE_A_2)
+    print(f"Started crawl at: {source_a_2_crawler.base_url}")
+    data = source_a_2_crawler.handle()
+    current_date = datetime.now().strftime("%H_%M__%d_%m_%Y")
+    filename = f"source_a_2_{current_date}.csv"
     write_json_to_csv(filename, data)
 
 
